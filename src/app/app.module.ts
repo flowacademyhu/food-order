86b59f58cbcc7d9ebf352dac4fcba4319b1551af
Ybira <koszotam@gmail.com>
2018-09-11 09:57:18 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrderBoardComponent } from './order-board/order-board.component';
+import { FoodListComponent } from './food-list/food-list.component';
+import { FoodComponent } from './food/food.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    OrderBoardComponent,
+    FoodListComponent,
+    FoodComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/order-board/order-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-board',
+  templateUrl: './order-board.component.html',
+  styleUrls: ['./order-board.component.css']
+})
+export class OrderBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
