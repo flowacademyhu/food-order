081499df852a5a8cc1025e042804816b87308cca
Gabor Thury <thury.gabor1994@gmail.com>
2018-09-11 11:53:19 +0200

    made routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { OrderBoardComponent } from './order-board/order-board.component';
 import { FoodListComponent } from './food-list/food-list.component';
 import { FoodComponent } from './food/food.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: OrderBoardComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
@@ -18,12 +24,13 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     FooterComponent,
     OrderBoardComponent,
     FoodListComponent,
-    FoodComponent
+    FoodComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule,
+    RouterModule.forRoot(routes),
     NgbModule
   ],
   providers: [],
